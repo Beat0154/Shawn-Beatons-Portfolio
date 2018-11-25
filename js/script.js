630c5bfcae27908845e23215684b8c65d6c3c7f5
Shawn Beaton <beat0154@algonquinlive.com>
2018-11-25 14:06:44 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+$("#greycss").on('click', function () {
+    $("link").attr(href, better);
+});
