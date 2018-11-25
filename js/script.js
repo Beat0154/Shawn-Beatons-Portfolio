c48092861f8163631e2a3bb676d2b414e1725e63
Shawn Beaton <beat0154@algonquinlive.com>
2018-11-25 15:20:18 -0500

    theme switch button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+function switchTheme() {
+  var check = document
+  .getElementById('theme_css')
+  .classList[0] === 'styles';
 
-$("#greycss").on('click', function () {
-    $("link").attr(href, better);
-});
+  var element = document.getElementById('theme_css');
+
+
+  if (check) {
+   element.href = 'css/better-styles.css';
+   element.classList.remove('styles')
+   element.classList.add('better-styles');
+  } else {
+   element.href = 'css/styles.css';
+   element.classList.remove('better-styles')
+   element.classList.add('styles');
+  }
+
+}
